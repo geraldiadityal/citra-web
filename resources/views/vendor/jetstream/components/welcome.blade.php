fec9d5154e95b2cf579f1b0d0792198779b7c85c
Geraldi Aditya <56405395+geraldiadityal@users.noreply.github.com>
2022-07-24 15:44:44 +0700

    fix dashboard for transaction

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -35,7 +35,7 @@
     <div class="p-6 border-t border-gray-200 md:border-l">
         <div class="flex items-center">
 
-            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="dashboard/room">Room Chats</a></div>
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="dashboard/transaction">Transaction</a></div>
         </div>
 
 
